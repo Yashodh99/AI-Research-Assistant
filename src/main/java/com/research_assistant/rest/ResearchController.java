feb1be887052495b5beec53fa7b28cd3fa704b51
yash.fern99 <yash.fern99@gmail.com>
2025-11-21 15:56:03 +0530

    added files

---
+++ src/main/java/com/research_assistant/rest/ResearchController.java
@@ -0,0 +1,24 @@
+package com.research_assistant.rest;
+
+
+import com.research_assistant.ResearchRequest;
+import com.research_assistant.service.ResearchService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/research")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
+public class ResearchController {
+
+    private final ResearchService researchService;
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processContent(@RequestBody ResearchRequest researchRequest){
+        String result = researchService.processContent(researchRequest);
+        return ResponseEntity.ok(result);
+
+    }
+}
---
+++ src/main/java/com/research_assistant/service/ResearchService.java
@@ -0,0 +1,8 @@
+package com.research_assistant.service;
+
+import com.research_assistant.ResearchRequest;
+
+public class ResearchService {
+    public String processContent(ResearchRequest researchRequest) {
+    }
+}
